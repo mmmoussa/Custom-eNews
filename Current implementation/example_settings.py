1035e2a547265f24dc6a3c589ed98d3e1dfe8c69
Mohamed <mmmoussa@uwaterloo.ca>
2015-04-25 00:32:32 -0400

    Added example_settings.py to use for configuration

---
+++ Current implementation/example_settings.py
@@ -0,0 +1,7 @@
+flaggedWords = ["these", "are", "some example", "flagged word", "you can use", 
+				"all words are automatically tested with a trailing s as well",
+				"so flagged word also includes flagged words", "words are processed in", 
+				"lowercase"]
+api_user = "your sendgrid api username"
+api_key = "your sendgrid api password"
+user_email = "Your sendgrid user email, which is the to and from address."
